6ff15b8ae2627a9898c7d5fe57b5630bdfc8417d
Chris Lincoln <chris.lincoln12@gmail.com>
2019-07-02 18:02:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,6 +41,14 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
   
+  def turn_count(board)
+  x_count = 0
+  o_count = 0
+  
+  board.each do |b|
+    x_count += 1 if b == "X"
+    o
+  
   
 end
 
