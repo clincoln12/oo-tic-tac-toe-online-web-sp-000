ac051d9fd3627f7b90be62ba52ed8c9f10139dfd
Chris Lincoln <chris.lincoln12@gmail.com>
2019-07-02 18:58:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,7 +66,7 @@ class TicTacToe
     index = input_to_index(input)
       if valid_move?(index)
         move(index, current_player)
-        move(index, "X" || "O")
+        
         display_board
       else
         turn
